ffb0792aaca3c19d3fd23e21546ad46500e0f982
DAVID_ALEJANDRO_ARELLANO_GAMON <a.21308051280429@cbtis128.edu.mx>
2024-05-02 19:13:30 +0000

    ac

---
+++ lib/data/product_data.dart
@@ -0,0 +1,16 @@
+import 'package:arellano0429/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/descarga.png",
+      title: "Ipohne 1",
+      description: "Burgers not"),
+  ProductModel(
+      img: "assets/descarga.png",
+      title: "Ipohne 2",
+      description: "Burgers not"),
+  ProductModel(
+      img: "assets/descarga.png",
+      title: "Iphone 3",
+      description: "Burgers not"),
+];
